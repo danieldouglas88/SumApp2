752f3a9d018333f3be6d0d97ada364de02d86987
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2018-02-05 20:58:39 -0800

    Initial commit

---
+++ app/src/main/java/com/daniel/sumapp/Activity2.java
@@ -0,0 +1,29 @@
+package com.daniel.sumapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(MainActivity.EXTRA_TEXT);
+        String number = intent.getStringExtra(MainActivity.EXTRA_NUMBER);
+
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        TextView textView3 = (TextView) findViewById(R.id.textView3);
+        TextView textView6 = (TextView) findViewById(R.id.textView6);
+
+        textView2.setText(text);
+        textView3.setText(number);
+
+        textView6.setText(text+number);
+
+    }
+}
